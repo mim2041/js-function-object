e8001909814a5e5af38aa765bd1bc16a7b5a0995
mim2041 <suraiyamim338@gmail.com>
2023-01-29 21:36:03 +0600

    Welcome to function

---
+++ function.js
@@ -0,0 +1,11 @@
+// function declaration
+function startFan(){
+    console.log('Stand up');
+    console.log('walk toward the switch');
+    console.log('press the switch');
+}
+
+// call the function
+startFan();
+console.log('Walking up in the morning');
+startFan();
