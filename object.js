645c5eb64b3bbcf3fe69e879ad65d97c25624d10
mim2041 <suraiyamim338@gmail.com>
2023-01-29 23:16:21 +0600

    object and switch practice

---
+++ object.js
@@ -0,0 +1,24 @@
+var student = { id: 115, 
+    name: 'Solaiman Khan', 
+    class: 9, 
+    marks: 98
+};
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    storage: '64gb',
+    camera: '7MP'
+}
+
+var myComputer = {
+    brand: 'lenovo',
+    price: 39000,
+    color: 'silver',
+    processor: 'i7'
+}
+
+console.log(myComputer);
+// console.log(myComputer.color);
+myComputer.processor = 'i79';
+console.log(myComputer);
---
+++ properties.js
@@ -0,0 +1,30 @@
+var shoppingCart = {
+    books: 3,
+    sunglass: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25
+}
+
+var penCount = shoppingCart.pen;
+// console.log(penCount);
+
+var penCount2 = shoppingCart['pen'];
+
+var properties = Object.keys(shoppingCart);
+// console.log(properties);
+
+var propertyValues = Object.values(shoppingCart);
+// console.log(propertyValues);
+
+var propertyName = 'mouse';
+var propertyValue = shoppingCart[propertyName];
+// console.log(propertyValue);
+
+// set property values
+shoppingCart.mouse = 15;
+console.log(shoppingCart);
+shoppingCart['mouse'] = 29;
+console.log(shoppingCart);
+shoppingCart[propertyName] = 89;
+console.log(shoppingCart);
